f0667a4ef60211fa5319463e310c48c2957a5985
Loperamayday <123569796+Loperamayday@users.noreply.github.com>
2023-02-08 10:20:52 -0500

    cambios-8-02-23

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj =  [ 
+ {
+    name: "tiburon",
+ imagenes: "imagenes/tiburon[1].jpg"
+ }
+ {
+  name: "tiburon",
+imagenes: "imagenes/tiburon[2].jpg"
+}
+{
+  name: "tiburon",
+imagenes: "imagenes/tiburon[3].jpg"
+}
+{
+  name: "tiburon",
+imagenes: "imagenes/tiburon[4].jpg"
+}
+{
+  name: "tiburon",
+imagenes: "imagenes/tiburon[5].jpg"
+}
+{
+  name: "tiburon",
+imagenes: "imagenes/tiburon[6].jpg"
+}
+{
+  name: "tiburon",
+imagenes: "imagenes/tiburon[1].jpg"
+}
+{
+name: "tiburon",
+imagenes: "imagenes/tiburon[2].jpg"
+}
+{
+name: "tiburon",
+imagenes: "imagenes/tiburon[3].jpg"
+}
+{
+name: "tiburon",
+imagenes: "imagenes/tiburon[4].jpg"
+}
+{
+name: "tiburon",
+imagenes: "imagenes/tiburon[5].jpg"
+}
+{
+name: "tiburon",
+imagenes: "imagenes/tiburon[6].jpg"
+    }
+  ];
+});
